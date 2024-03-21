36384202482d2d86daeb092fb508281ff20444be
Noah Bussinger <development@palto.ch>
2024-03-21 11:06:13 +0100

    Initial reindentation and style config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "styles/index.scss";
 
 const App = () => {
-  return <div>SoPra Lab FS24 Group 26 Client</div>;
+    return <div>SoPra Lab FS24 Group 26 Client</div>;
 };
 
 export default App;
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,10 +1,10 @@
 export const getDomain = () => {
-  const prodUrl = "https://sopra-fs24-group-26-server.oa.r.appspot.com/";
-  const devUrl = "http://localhost:8080";
+    const prodUrl = "https://sopra-fs24-group-26-server.oa.r.appspot.com/";
+    const devUrl = "http://localhost:8080";
 
-  return isProduction() ? prodUrl : devUrl;
+    return isProduction() ? prodUrl : devUrl;
 };
 
 export const isProduction = () => {
-  return process.env.NODE_ENV === "production";
+    return process.env.NODE_ENV === "production";
 };
