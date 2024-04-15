7775ec9eac67c04493bedd74f1eba7810a0e33dd
Roxane <roxane.jaecklin@gmail.com>
2024-04-15 22:31:04 +0200

    59: Added Titlescreen

--- src/scenes/titlescreen.ts
+++ src/scenes/titlescreen.ts
@@ -1,37 +1,45 @@
-import { Nullable, UUID } from "definitions/utils";
+import { float, Nullable } from "definitions/utils";
 import SessionManager from "managers/SessionManager";
 import Phaser from "phaser";
 import { interactify } from "../utilities/utils";
 import { ScreenHeight, ScreenWidth } from "../core/main";
+import Image = Phaser.GameObjects.Image;
 
 export class TitleScreen extends Phaser.Scene {
-    private title: Nullable<Phaser.GameObjects.Text>;
     private button: Nullable<Phaser.GameObjects.Image>;
 
     public constructor() {
         super("TitleScreen");
-        this.title = null;
         this.button = null;
     }
 
     public preload(): void {
         this.load.image("createLobby", "assets/create.png");
+        this.load.image("titlescreen", "assets/sabo.png");
     }
 
     public create(): void {
-        this.title = this.add
-            .text(ScreenWidth / 2, ScreenHeight / 4, "Saboteur", {
-                font: "Arial Black",
-                fontSize: 38,
-                color: "#ffffff",
-                stroke: "#000000",
-                strokeThickness: 8,
-                align: "center",
-            })
-            .setOrigin(0.5);
-        this.button = this.add.image(
+        this.add
+            .rectangle(0, 0, ScreenWidth, ScreenHeight, 0x000000)
+            .setOrigin(0);
+
+        const backgroundImage: Image = this.add.image(
             ScreenWidth / 2,
-            ScreenHeight / 1.5,
+            ScreenHeight / 2,
+            "titlescreen",
+        );
+
+        const maxWidthScale: float = ScreenWidth / backgroundImage.width;
+        const maxHeightScale: float = ScreenHeight / backgroundImage.height;
+        const scale: float = Math.min(maxWidthScale, maxHeightScale);
+        backgroundImage.setScale(scale);
+
+        // Center the image
+        backgroundImage.setPosition(ScreenWidth / 2, ScreenHeight / 2);
+
+        this.button = this.add.image(
+            ScreenWidth / 1.4,
+            ScreenHeight / 1.1,
             "createLobby",
         );
         interactify(this.button, 0.2, () => this.onButton());
