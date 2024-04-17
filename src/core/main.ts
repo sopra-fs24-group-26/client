e149b9b82ff9a977829506e4fd5d895bd36addca
Croxsy <147318295+Croxsy@users.noreply.github.com>
2024-04-17 19:59:26 +0200

    59: Add titlescreen (#67)
    * 59: Added Titlescreen
    
    * 59: Fixed minor issues, with exception of the titlescreen
    
    * 59: Fix titlescreen size
    
    * 59: Fix typo

--- src/core/main.ts
+++ src/core/main.ts
@@ -15,7 +15,7 @@ const config: Phaser.Types.Core.GameConfig = {
     width: ScreenWidth,
     height: ScreenHeight,
     parent: "game-container",
-    backgroundColor: "#028af8",
+    backgroundColor: "black",
     scene: [TitleScreen, LobbyScreen],
 };
 
