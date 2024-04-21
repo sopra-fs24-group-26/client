a57081ea80b707abb8b2f257b55b0667d78a57a8
Croxsy <147318295+Croxsy@users.noreply.github.com>
2024-04-21 23:24:44 +0200

    5: Players have a visual representation of tiles in Hand
    * 5: Add displayDrawnTiles
    
    * 5: Add Tiles into Container
    
    * 5: Rebase onto master, remove mockfunction
    
    * 5: Remove obsolete assert

--- src/scenes/gameuiscreen.ts
+++ src/scenes/gameuiscreen.ts
@@ -1,32 +1,67 @@
 import { ScreenHeight, ScreenWidth } from "core/main";
 import Phaser from "phaser";
-import { log } from "utilities/logger";
-import { interactify } from "utilities/utils";
+import { assert } from "utilities/utils";
+import { int, Nullable, UUID } from "../definitions/utils";
+import { Tile } from "../entities/Tile";
+import TileManager from "../managers/TileManager";
 
 export class GameUiScreen extends Phaser.Scene {
+    private uiBackground: Nullable<Phaser.GameObjects.Rectangle>;
+    private drawnTilesContainer: Nullable<Phaser.GameObjects.Container>;
+
     public constructor() {
         super("GameUiScreen");
+        this.uiBackground = null;
+        this.drawnTilesContainer = null;
+    }
+
+    public init(): void {
+        const tileUpdateListener: UUID = TileManager.onSync.on(() => {
+            this.displayDrawnTiles();
+        });
+        this.events.on("destroy", () => {
+            TileManager.onSync.off(tileUpdateListener);
+        });
     }
 
     public preload(): void {
-        this.load.image("start", "assets/buttons/start.png");
+        for (let i: int = 0; i < 9; i++) {
+            this.load.image(`tile${i}`, `assets/tiles/tile${i}.png`);
+        }
     }
 
     public create(): void {
-        const back: Phaser.GameObjects.Rectangle = this.add.rectangle(
+        this.uiBackground = this.add.rectangle(
             ScreenWidth / 2,
             ScreenHeight - 100,
             ScreenWidth * 0.8,
             200,
-            0xffffff,
+            0x808080,
         );
-        back.setInteractive();
+        this.uiBackground.setInteractive();
+        this.drawnTilesContainer = this.add.container();
+    }
 
-        const button: Phaser.GameObjects.Image = this.add.image(
-            ScreenWidth / 2,
-            ScreenHeight - 100,
-            "start",
-        );
-        interactify(button, 0.5, () => log("button"));
+    public displayDrawnTiles(): void {
+        assert(this.drawnTilesContainer);
+        this.drawnTilesContainer.removeAll(true);
+        const myTiles: Nullable<Tile[]> = TileManager.getInHand();
+        assert(myTiles && this.uiBackground);
+
+        const nrTiles: int = myTiles.length;
+        const tileSpacing: int =
+            (this.uiBackground.width - nrTiles * 128) / (nrTiles + 1);
+
+        for (let i: int = 0; i < nrTiles; i++) {
+            this.drawnTilesContainer.add(
+                this.add.image(
+                    this.uiBackground.getTopLeft().x +
+                        (tileSpacing + 128 / 2) +
+                        i * (128 + tileSpacing),
+                    ScreenHeight - 100,
+                    `tile${myTiles[i].type}`,
+                ),
+            );
+        }
     }
 }
