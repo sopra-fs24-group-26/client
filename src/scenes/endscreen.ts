6722226bd2dd0aff89b69319c6d899928cbc8613
Patertuck <patic.brandao@hispeed.ch>
2024-05-13 10:03:32 +0200

    124: Add ore shower endscreen

--- src/scenes/endscreen.ts
+++ src/scenes/endscreen.ts
@@ -14,11 +14,13 @@ export class EndScreen extends Phaser.Scene {
 
     public preload(): void {
         this.load.image("quit", "assets/buttons/quit.png");
+        this.load.image("goldNugget", "assets/particles/goldNugget.png");
+        this.load.image("coalNugget", "assets/particles/coalNugget.png");
     }
 
     public create(): void {
-        this.displayMessage();
-        PlayerManager.removeId();
+        this.displayByRole();
+        //PlayerManager.removeId();
 
         const button: Phaser.GameObjects.Image = this.add
             .image(ScreenWidth / 2, ScreenHeight / 1.5, "quit")
@@ -30,17 +32,19 @@ export class EndScreen extends Phaser.Scene {
         this.scene.start("TitleScreen");
     }
 
-    private displayMessage(): void {
+    private displayByRole(): void {
         let text: string = "You loose";
+        let showerOre: string = "coalNugget";
         const me: Nullable<Player> = PlayerManager.getMe();
         assert(me);
-        const minerWin: boolean =
-            me.role === Role.Miner && SessionManager.getReachedGold();
-        const saboteurWin: boolean =
-            me.role === Role.Saboteur && !SessionManager.getReachedGold();
-        if (minerWin || saboteurWin) {
+        if (
+            (me.role === Role.Miner && SessionManager.getReachedGold()) ||
+            (me.role === Role.Saboteur && !SessionManager.getReachedGold())
+        ) {
+            showerOre = "goldNugget";
             text = "You win baby";
         }
+        this.createShower(showerOre);
         this.add
             .text(ScreenWidth / 2, ScreenHeight / 2, text, {
                 fontFamily: "Arial",
@@ -49,5 +53,17 @@ export class EndScreen extends Phaser.Scene {
                 fontStyle: "bold",
             })
             .setOrigin(0.5, 0.5);
+    }
+
+    private createShower(showerOre: string): void {
+        this.add.particles(0, 100, showerOre, {
+            x: { min: 0, max: ScreenWidth },
+            y: -400,
+            accelerationY: 500,
+            maxVelocityY: 700,
+            quantity: 1,
+            lifespan: 5000,
+            gravityY: 200,
+        });
     }
 }
